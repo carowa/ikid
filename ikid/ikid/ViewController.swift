5203a60555ac2bb945b886f75e9e34f6fe9cdf23
August Carow <august.carow@gmail.com>
2018-02-02 18:21:09 -0800

    make initial story board

---
+++ ikid/ikid/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ikid
+//
+//  Created by August Carow on 2/2/18.
+//  Copyright © 2018 August Carow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
