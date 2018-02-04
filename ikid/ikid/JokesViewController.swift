4a313c2d3926a72b83d000731517e6868ed234fa
August Carow <august.carow@gmail.com>
2018-02-04 12:21:19 -0800

    storyboard made

---
+++ ikid/ikid/JokesViewController.swift
@@ -0,0 +1,30 @@
+//  August Carow
+//  ViewController.swift
+//  ikid
+//
+//  Created by August Carow on 2/4/18.
+//  Copyright © 2018 August Carow. All rights reserved.
+//
+
+import UIKit
+
+class JokesViewController: UIViewController {
+
+    @IBOutlet weak var knockKnock: UILabel!
+    
+    @IBAction func flip(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
