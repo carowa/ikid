dc53c429198a70536bf3d7a5f9377bb75c66cce0
August Carow <august.carow@gmail.com>
2018-02-04 11:49:12 -0800

    changing view controllers

--- ikid/ikid/ViewController.swift
+++ ikid/ikid/ViewController.swift
@@ -1,4 +1,4 @@
-//
+//  August Carow
 //  ViewController.swift
 //  ikid
 //
@@ -8,8 +8,8 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITabBarController {
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +19,6 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
